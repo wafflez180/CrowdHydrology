30029cd06db1dde605e010dcea15bd6c651fb887
Arthur De Araujo <wafflez180@yahoo.com>
2018-07-16 14:35:33 -0400

    Customized Django admin page

--- main_app/admin.py
+++ main_app/admin.py
@@ -3,7 +3,26 @@ from main_app.models import SMSContribution, InvalidSMSContribution, Station, Sp
 
 # Register your models here.
 
-admin.site.register(SMSContribution)
-admin.site.register(InvalidSMSContribution)
-admin.site.register(Station)
-admin.site.register(Sponsor)
+class SMSContributionAdmin(admin.ModelAdmin):
+    search_fields = ['contributor_id', 'station', 'water_height', 'temperature', 'date_received']
+    list_filter = ['station']
+    list_display = ['contributor_id', 'water_height', 'temperature', 'station', 'date_received']
+
+class InvalidSMSContributionAdmin(admin.ModelAdmin):
+    search_fields = ['contributor_id', 'message_body']
+    list_display = ['contributor_id', 'message_body', 'date_received']
+
+class StationAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'name', 'state']
+    list_filter = ['status', 'water_body_type', 'state']
+    list_display = ['id', 'name', 'state', 'water_body_type', 'status', 'date_added']
+    #list_editable = ['status']
+
+class SponsorAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name']
+
+admin.site.register(SMSContribution, SMSContributionAdmin)
+admin.site.register(InvalidSMSContribution, InvalidSMSContributionAdmin)
+admin.site.register(Station, StationAdmin)
+admin.site.register(Sponsor, SponsorAdmin)
